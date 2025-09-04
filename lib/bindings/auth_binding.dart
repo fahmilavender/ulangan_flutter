6cc9c29db0c666750c765e49e8dd8520e725b6a7
KingSolid <raffadityaaw23@gmail.com>
2025-09-05 06:22:15 +0700

    Controllers

--- lib/bindings/auth_binding.dart
+++ lib/bindings/auth_binding.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 import 'package:ulangan_flutter/controllers/auth_controllers.dart';
+import 'package:ulangan_flutter/controllers/home_controller.dart';
+import 'package:ulangan_flutter/controllers/todo_controller.dart';
 
 class AuthBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(AuthController());
+    Get.put<AuthController>(AuthController());
+    Get.put<TodoController>(TodoController());
+    Get.put<HomeController>(HomeController());
   }
 }
---
+++ lib/controllers/todo_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+class TodoController extends GetxController {
+  // Model todo sederhana
+  var todos = <Map<String, dynamic>>[].obs;
+
+  void addTodo(String title) {
+    todos.add({
+      "title": title,
+      "isDone": false,
+    });
+  }
+
+  void toggleDone(int index) {
+    todos[index]["isDone"] = !(todos[index]["isDone"] as bool);
+    todos.refresh();
+  }
+
+  void deleteTodo(int index) {
+    todos.removeAt(index);
+  }
+}
