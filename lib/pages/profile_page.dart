a7e0e7b63cd5b5876029ad0b1fbfd99cc2d5b58f
KingSolid <raffadityaaw23@gmail.com>
2025-09-13 17:21:16 +0700

    Add ProfilePage

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:ulangan_flutter/components/customcolors.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile Page (Kosong dulu)"),
+    return Scaffold(
+      backgroundColor: Customcolors.white,
+      appBar: AppBar(
+        title: const Text("Profile Page"),
+        backgroundColor: Customcolors.bluecontainer,
+        foregroundColor: Customcolors.white,
+        centerTitle: true,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/Raff.jpg"),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nama: Raffaditya Alvaro",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text("Kelas: 11 PPLG 2"),
+                      Text("Absen: 28"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage("https://i.pravatar.cc/150?img=4"),//tar ganti
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nama: -",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text("Kelas: 11 PPLG 2"),
+                      Text("Absen: -"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
